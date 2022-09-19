a09176e5f2c6ba6a4af8f9e5c2145883da945dc8
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2022-09-19 19:20:03 +0800

    Implement household model and accompanying create route

--- src/grants/households/routes.py
+++ src/grants/households/routes.py
@@ -1,7 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from grants.models import Household
+from grants import db
 
 households = Blueprint('households', __name__)
 
 @households.route('/household/new', methods=['POST'])
 def create_household():
+    housing_type = request.form.get('Housing Type')
+    
+    household = Household(housing_type=housing_type)
+    db.session.add(household)
+    db.session.commit()
     return {}
--- tests/test_grants.py
+++ tests/test_grants.py
@@ -1,11 +1,25 @@
-from grants import create_app
+from grants import create_app, db
+import pytest
+from grants.models import Household
 
 
-def test_create_household():
-    app = create_app()
-    with app.test_client() as test_app:
-        data = {'Housing Type': 'Landed'}
-        response = test_app.post('/household/new', data=data)
-        assert response.status_code == 200
+@pytest.fixture
+def client():
+    app = create_app(True)
+    with app.app_context():
+        db.create_all()
+        with app.test_client() as test_app:
+            yield test_app
+        db.drop_all()
 
-        # TODO: Check that household has been created in the DB
+@pytest.mark.parametrize('housing_type', ['Landed', 'Condominium', 'HDB'])
+def test_create_household(client, housing_type):
+    data = {
+        'Housing Type': housing_type
+    }
+    response = client.post('/household/new', data=data)
+    assert response.status_code == 200
+
+    household = Household.query.get(1)
+    assert household.housing_type == housing_type
+
