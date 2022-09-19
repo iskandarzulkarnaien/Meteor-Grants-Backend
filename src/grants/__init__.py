4b714379b3946d21569c8da1f20a441e3bcd889d
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2022-09-19 18:20:24 +0800

    Implement route for create_household

--- src/grants/__init__.py
+++ src/grants/__init__.py
@@ -3,11 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://site.db'
 
-
     db.init_app(app)
+
+    from grants.households.routes import households
+
+    app.register_blueprint(households)
 
     return app
--- tests/test_grants.py
+++ tests/test_grants.py
@@ -1,5 +1,11 @@
-import grants
+from grants import create_app
 
 
-def test_placeholder():
-    assert grants.__name__ == grants.__name__
+def test_create_household():
+    app = create_app()
+    with app.test_client() as test_app:
+        data = {'Housing Type': 'Landed'}
+        response = test_app.post('/household/new', data=data)
+        assert response.status_code == 200
+
+        # TODO: Check that household has been created in the DB
