2a14340ef7f5a10c691a8e63f592391935e11110
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2022-09-19 21:46:02 +0800

    Shift database creation to end of app initialization

--- src/grants/__init__.py
+++ src/grants/__init__.py
@@ -7,17 +7,17 @@ db = SQLAlchemy()
 def create_app(test=False):
     app = Flask(__name__)
 
+    db.init_app(app)
+
     if test:
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.site.sqlite'
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite'
 
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
     from grants.households.routes import households
 
     app.register_blueprint(households)
 
+    with app.app_context():
+        db.create_all()
     return app
