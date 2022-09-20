ff9f7e69680b4d765b74dc3c6d5f9516f29abe05
Iskandar Zulkarnaien <iskandar@u.nus.edu>
2022-09-20 16:25:42 +0800

    Shift pytest fixtures to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,41 @@
 import sys
 import os
+import pytest
+from grants import create_app, db
+from helpers.utils import PersonBuilder, HouseholdBuilder
+
 sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
+
+
+@pytest.fixture
+def client():
+    app = create_app(True)
+    with app.app_context():
+        db.create_all()
+        with app.test_client() as test_app:
+            test_app.app_context = app.test_request_context()
+            test_app.app_context.push()
+            yield test_app
+        db.drop_all()
+
+
+@pytest.fixture
+def empty_household():
+    empty_household = HouseholdBuilder().create()
+    return empty_household
+
+
+@pytest.fixture
+def empty_household_saved():
+    return HouseholdBuilder().create_and_write()
+
+
+@pytest.fixture
+def person(empty_household):
+    person = PersonBuilder(empty_household).create()
+    return person
+
+
+@pytest.fixture
+def person_saved(empty_household_saved):
+    return PersonBuilder(empty_household_saved).create_and_write()
--- tests/test_grants.py
+++ tests/test_grants.py
@@ -1,44 +1,9 @@
-from grants import create_app, db
+from grants import db
 import pytest
 from grants.models import Household, Person
 from datetime import datetime, timedelta
 from flask import url_for
 import json
-from helpers.utils import PersonBuilder, HouseholdBuilder
-
-
-@pytest.fixture
-def client():
-    app = create_app(True)
-    with app.app_context():
-        db.create_all()
-        with app.test_client() as test_app:
-            test_app.app_context = app.test_request_context()
-            test_app.app_context.push()
-            yield test_app
-        db.drop_all()
-
-
-@pytest.fixture
-def empty_household():
-    empty_household = HouseholdBuilder().create()
-    return empty_household
-
-
-@pytest.fixture
-def empty_household_saved():
-    return HouseholdBuilder().create_and_write()
-
-
-@pytest.fixture
-def person(empty_household):
-    person = PersonBuilder(empty_household).create()
-    return person
-
-
-@pytest.fixture
-def person_saved(empty_household_saved):
-    return PersonBuilder(empty_household_saved).create_and_write()
 
 
 # Tests For API 1
